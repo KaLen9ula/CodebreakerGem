c2b41f1dc312dcaf63526e8e1d72f556dc48ca56
KaLen9ula <andruhovickola167@gmail.com>
2021-10-11 17:52:09 +0300

    Refactor according to rubocop rules

--- lib/codebreaker_gem/stats.rb
+++ lib/codebreaker_gem/stats.rb
@@ -16,9 +16,13 @@ module Codebreaker
 
     def group_statistics(data)
       data.map.with_index do |game, index, name = game.user.name|
-        {number: index + 1, name: name, difficulty: game.difficulty,
-         available_attempts: Settings::DIFFICULTIES[Settings::DIFFICULTIES.keys[game.difficulty]][:attempts], used_attempts: game.user.attempts,
-         available_hints: Settings::DIFFICULTIES[Settings::DIFFICULTIES.keys[game.difficulty]][:hints], used_hints: game.user.hints}
+        { number: index + 1,
+          name: name,
+          difficulty: game.difficulty,
+          available_attempts: Settings::DIFFICULTIES[Settings::DIFFICULTIES.keys[game.difficulty]][:attempts],
+          used_attempts: game.user.attempts,
+          available_hints: Settings::DIFFICULTIES[Settings::DIFFICULTIES.keys[game.difficulty]][:hints],
+          used_hints: game.user.hints }
       end
     end
   end
